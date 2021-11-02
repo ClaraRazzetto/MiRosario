5fcf574a854c83b9d556aab63080c6dcacfc6f51
Clara Razzetto <clararazzetto@gmail.com>
2021-11-01 21:44:18 -0300

    configuracion Spring Security y entidad cliente

--- MiRosario/src/main/java/com/mirosario/MiRosario/repositorios/UsuarioRepositorio.java
+++ MiRosario/src/main/java/com/mirosario/MiRosario/repositorios/UsuarioRepositorio.java
@@ -2,9 +2,14 @@ package com.mirosario.MiRosario.repositorios;
 
 import com.mirosario.MiRosario.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, String>{
+    
+    @Query("SELECT u FROM Usuario u WHERE u.nombreUsuario =: nombreUsuario")
+    public Usuario buscarNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
 
 }
