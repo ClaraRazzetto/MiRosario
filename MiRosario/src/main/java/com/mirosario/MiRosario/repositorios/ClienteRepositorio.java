7fcb97504a440fc1a39f2a1226b1b190723ee575
Clara Razzetto <clararazzetto@gmail.com>
2021-11-02 20:49:50 -0300

    servicio cliente con algunos metodos

--- MiRosario/src/main/java/com/mirosario/MiRosario/repositorios/ClienteRepositorio.java
+++ MiRosario/src/main/java/com/mirosario/MiRosario/repositorios/ClienteRepositorio.java
@@ -2,9 +2,16 @@ package com.mirosario.MiRosario.repositorios;
 
 import com.mirosario.MiRosario.entidades.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepositorio extends JpaRepository<Cliente, String>{
 
+    @Query("SELECT c FROM Cliente c WHERE c.nombreUsuario =: nombreUsuario")
+    public Cliente buscarNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
+    
+    @Query("SELECT c FROM Cliente c WHERE c.dni =: dni")
+    public Cliente buscarDni(@Param("dni") String dni);
 }
