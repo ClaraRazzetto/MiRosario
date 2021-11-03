2bff4c7936cb603e957b7f9be0ca9db1aafc657f
Angel Cuenca <Angel_Cuenca_95@hotmail.com>
2021-11-03 18:35:56 -0300

    Resolviendo el conflicto en comentario

--- MiRosario/src/main/java/com/mirosario/MiRosario/servicios/ProductoServicio.java
+++ MiRosario/src/main/java/com/mirosario/MiRosario/servicios/ProductoServicio.java
@@ -29,12 +29,13 @@ public class ProductoServicio {
                         return productoRepositorio.save(producto);
     }
     @Autowired
-    public Producto editar(String id, String nombre, Double precio, String descripcion, MultipartFile archivo) throws ErrorServicio{
+    public Producto editar(String id, String nombre, Double precio, String descripcion, MultipartFile archivo,String idFoto) throws ErrorServicio{
         validacion(nombre, precio, descripcion);
         Producto producto=findById(nombre);
         producto.setNombre(nombre);
         producto.setPrecio(precio);
         producto.setDescripcion(descripcion);
+        producto.setFoto(fotoServicio.editar(idFoto, archivo));
         
     return productoRepositorio.save(producto);
     }
