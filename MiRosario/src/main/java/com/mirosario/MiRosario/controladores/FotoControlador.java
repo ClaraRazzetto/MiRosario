e4670b60bfd4498b6964aaa782fcf5a304e1f6bd
Angel Cuenca <Angel_Cuenca_95@hotmail.com>
2021-11-11 19:33:34 -0300

    agregados los metodos en el controlador de foto

--- MiRosario/src/main/java/com/mirosario/MiRosario/controladores/FotoControlador.java
+++ MiRosario/src/main/java/com/mirosario/MiRosario/controladores/FotoControlador.java
@@ -2,13 +2,14 @@ package com.mirosario.MiRosario.controladores;
 
 import com.mirosario.MiRosario.entidades.Cliente;
 import com.mirosario.MiRosario.entidades.Comercio;
-import com.mirosario.MiRosario.entidades.Usuario;
+import com.mirosario.MiRosario.entidades.Producto;
+
 import com.mirosario.MiRosario.excepciones.ErrorServicio;
 import com.mirosario.MiRosario.servicios.ClienteServicio;
 import com.mirosario.MiRosario.servicios.ComercioServicio;
+import com.mirosario.MiRosario.servicios.ProductoServicio;
 import com.mirosario.MiRosario.servicios.UsuarioServicio;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,41 +17,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/foto")
 public class FotoControlador {
-<<<<<<< HEAD
- @Autowired
- private ClienteServicio clienteServicio;
- @Autowired
- private ComercioServicio comercioServicio;
- @Autowired
- private UsuarioServicio usuarioServicio;
- @GetMapping("/usuario")
- public ResponseEntity<byte[]>fotoUsuario(String idUsuario){
-          try {
-              Cliente cliente= clienteServicio.findById(idUsuario);
-                 byte[]foto= cliente.getFoto().getContenido();
-                 HttpHeaders headers = new HttpHeaders();
-                 headers.setContentType(MediaType.IMAGE_JPEG);
-                 
-                 return new ResponseEntity<>(foto, headers, HttpStatus.OK);
-                      
-                      } catch (ErrorServicio ex) {
-              System.out.println("error"+ex.getMessage());
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-          }
-     
- }
-=======
->>>>>>> 46520a31b9329db4cb2d4c0b945d41b3e7595385
 
     @Autowired
     private ClienteServicio clienteServicio;
@@ -58,17 +33,62 @@ public class FotoControlador {
     private ComercioServicio comercioServicio;
     @Autowired
     private UsuarioServicio usuarioServicio;
+    @Autowired
+    private ProductoServicio productoServicio;
 
     @GetMapping("/cliente")
-    public ResponseEntity<byte[]> fotoCliente (@RequestParam String id, HttpSession sesion) {
+    public ResponseEntity<byte[]> fotoCliente(@RequestParam String id, HttpSession sesion) {
         try {
-            Cliente cliente = (Cliente) sesion.getAttribute("usuariosesion");;
-            if(cliente.getFoto() == null){
+//            Cliente cliente = (Cliente) sesion.getAttribute("usuariosesion");;
+                     Cliente cliente = clienteServicio.findById(id);
+            if (cliente.getFoto() == null) {
                 throw new ErrorServicio("El usuario no posee una foto asignada");
             }
-            
+
             byte[] foto = cliente.getFoto().getContenido();
-            
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            return new ResponseEntity<>(foto, headers, HttpStatus.OK);
+
+        } catch (ErrorServicio ex) {
+            System.out.println("error" + ex.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+      @GetMapping("/comercio")
+    public ResponseEntity<byte[]> fotoComercio(@RequestParam String id, HttpSession sesion) {
+        try {
+//            Cliente cliente = (Cliente) sesion.getAttribute("usuariosesion");;
+                     Comercio comercio = comercioServicio.findById(id);
+            if (comercio.getFoto() == null) {
+                throw new ErrorServicio("El usuario no posee una foto asignada");
+            }
+
+            byte[] foto = comercio.getFoto().getContenido();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            return new ResponseEntity<>(foto, headers, HttpStatus.OK);
+
+        } catch (ErrorServicio ex) {
+            System.out.println("error" + ex.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+      @GetMapping("/producto")
+    public ResponseEntity<byte[]> fotoProducto(@RequestParam String id, HttpSession sesion) {
+        try {
+//            Cliente cliente = (Cliente) sesion.getAttribute("usuariosesion");;
+                     Producto producto = productoServicio.findById(id);
+            if (producto.getFoto() == null) {
+                throw new ErrorServicio("El usuario no posee una foto asignada");
+            }
+
+            byte[] foto = producto.getFoto().getContenido();
+
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.IMAGE_JPEG);
 
